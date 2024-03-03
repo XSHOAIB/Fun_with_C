2524b80c9632510c8a40cca59bbf4e6096cdbaf2
XSHOAIB <145601395+XSHOAIB@users.noreply.github.com>
2024-03-03 14:08:02 +0530

    Create Quiz_Paper.c

---
+++ Quiz_Paper.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main (){
+	
+	printf("\t\t\t\t QUIZ PAPER\n\n--> Each Question Carries 4 Marks for Correct Answer and -1 for Wrong Answer\n\n");
+	
+	int n,n1, score=0;
+	printf("1Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf("2Q) when was india independent ? \n\n 1] 1955 \t\t 2] 1947 \n 3] 2004 \t\t 4] Never\n Answer : ");
+	scanf("%d",&n);
+    n == 2?printf("correct\n"):printf("wrong\n");
+	if (n == 2) {score += 4;} else {score--;}
+	
+	printf("3Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf("4Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf("5Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf("6Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf("7Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf("8Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf("9Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf("10Q) Who is the owner of TESLA ? \n\n 1] Elon Musk \t\t 2] Jeff Bezoz \n 3] Steve Jobs \t\t 4] Mark ZukerBurg\n Answer : ");
+	scanf("%d",&n);
+	n == 1?printf("correct\n"):printf("wrong\n");
+	if (n == 1) {score += 4;} else {score--;}
+	
+	printf(" your score is : %d",score);
+return 0;
+}
